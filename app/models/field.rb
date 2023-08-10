b19f129062d9ce024c21765ea7e2b51195875aba
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2023-08-10 15:09:28 +0200

    text_long added / design

--- app/models/field.rb
+++ app/models/field.rb
@@ -13,7 +13,7 @@ class Field < ApplicationRecord
 	validates_presence_of :name
 	validates_presence_of :datatype
 
-	enum datatype: [:Texte, :Nombre, :Euros, :Date, :Oui_non?, :Liste, :Formule, :Fichier, :Signature, :Gps, :Météo]
+	enum datatype: [:Texte, :Nombre, :Euros, :Date, :Oui_non?, :Liste, :Formule, :Fichier, :Signature, :Gps, :Météo, :Texte_long]
 	enum operation: [:Somme, :Moyenne]
 
 	scope :filtres, -> { where(filtre: true) }
