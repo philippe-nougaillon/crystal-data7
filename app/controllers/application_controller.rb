5b54457b87c341b9b080e01e216fb28d92cf0aa6
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2023-08-16 00:25:14 +0200

    mobile part 1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,6 @@ class ApplicationController < ActionController::Base
   before_action :detect_device_format
   before_action :set_layout_variables
 
-  layout :set_layout
-
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
     @current_user ||= User.find_by_authentication_token(cookies[:auth_token]) if cookies[:auth_token] && @current_user.nil?
@@ -26,15 +24,6 @@ private
       request.variant = :phone
     else
       request.variant = :desktop
-    end
-  end
-
-  def set_layout
-    case request.user_agent
-    when /iPhone/i, /Android/i && /mobile/i, /Windows Phone/i
-      "application_phone"
-    else
-      "application"
     end
   end
 
