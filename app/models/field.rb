d0b922710b8cda2267330d4a920aebf483f4b779
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2023-09-11 14:02:12 +0200

    richtext added

--- app/models/field.rb
+++ app/models/field.rb
@@ -16,7 +16,7 @@ class Field < ApplicationRecord
 	validates_presence_of :name
 	validates_presence_of :datatype
 
-	enum datatype: [:Texte, :Nombre, :Euros, :Date, :Oui_non?, :Liste, :Formule, :Fichier, :Texte_long, :Image, :Workflow, :URL, :Couleur, :GPS, :PDF, :Table]
+	enum datatype: [:Texte, :Nombre, :Euros, :Date, :Oui_non?, :Liste, :Formule, :Fichier, :Texte_long, :Image, :Workflow, :URL, :Couleur, :GPS, :PDF, :Table, :Texte_riche]
 	enum operation: [:Somme, :Moyenne]
 
 	scope :filtres, -> { where(filtre: true) }
