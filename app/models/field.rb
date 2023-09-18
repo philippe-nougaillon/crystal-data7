4c88a0bfe5ef4840060e64239d0a925144bc41be
pierre-emmanueldacquet <p-edacquet@hotmail.fr>
2023-09-18 09:49:13 +0200

    Utilisateur type added

--- app/models/field.rb
+++ app/models/field.rb
@@ -16,7 +16,7 @@ class Field < ApplicationRecord
 	validates_presence_of :name
 	validates_presence_of :datatype
 
-	enum datatype: [:Texte, :Nombre, :Euros, :Date, :Oui_non?, :Liste, :Formule, :Fichier, :Texte_long, :Image, :Workflow, :URL, :Couleur, :GPS, :PDF, :Table, :Texte_riche]
+	enum datatype: [:Texte, :Nombre, :Euros, :Date, :Oui_non?, :Liste, :Formule, :Fichier, :Texte_long, :Image, :Workflow, :URL, :Couleur, :GPS, :PDF, :Table, :Texte_riche, :Utilisateur]
 	enum operation: [:Somme, :Moyenne]
 	enum visibility: [:Liste_et_Détails, :Vue_Liste, :Vue_Détails]
 
