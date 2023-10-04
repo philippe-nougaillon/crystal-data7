2d757a146634885d2983c1d1b843e99f66204b16
Philippe NOUGAILLON <philippe.nougaillon@gmail.com>
2023-10-04 16:57:28 +0200

    bullet optims

--- app/services/collection_to_csv.rb
+++ app/services/collection_to_csv.rb
@@ -15,9 +15,7 @@ class CollectionToCsv < ApplicationService
         #updated_at = updated_at_list[index]
         cols = []
         @table.fields.each_with_index do | field,index |
-          if field.Signature? and values[index]
-            cols << "Signé"
-          elsif field.Collection?
+          if field.Collection?
             cols << (values[index] ? field.get_linked_table_record(values[index]).to_s.gsub("'", " ") : nil)
           else
             cols << (values[index] ? values[index].to_s.gsub("'", " ") : nil) 
