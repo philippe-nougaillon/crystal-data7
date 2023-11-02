98e859e042e523241b69a1f0a4eb4d5ca876250f
P-edacquet <pierreemmanuel.dacquet@gmail.com>
2023-11-02 14:32:14 +0100

    signature enhancement

--- app/pdfs/export_pdf.rb
+++ app/pdfs/export_pdf.rb
@@ -33,6 +33,9 @@ class ExportPdf
       table.fields.each_with_index do | field,index |
         if field.Collection?
           fields_to_export << field.get_linked_table_record(values[index])
+        elsif field.Signature?
+          value = values[index].blank? ? 'Pas signé' : 'Signé'
+          fields_to_export << value
         else
           fields_to_export << values[index]
         end
