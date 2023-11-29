2966e1b67896a92b4dc8cc0677afab337523096f
P-edacquet <pierreemmanuel.dacquet@gmail.com>
2023-11-29 09:49:20 +0100

    default email changed

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-	default from: '"CrystalDATA" <crystaldata@philnoug.com>'
+	default from: '"CrystalDATA" <contact@philnoug.com>'
 
 	def notification(table, items)
 		@items = items
