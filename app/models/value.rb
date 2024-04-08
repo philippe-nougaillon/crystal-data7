d7226c45cf5531ecb946a2e31ec0c8f64e99fdb2
Philippe NOUGAILLON <philippe.nougaillon@gmail.com>
2024-04-08 11:03:50 +0200

    Date conversion fix

--- app/models/value.rb
+++ app/models/value.rb
@@ -8,6 +8,15 @@ class Value < ApplicationRecord
   scope :records_at, ->(i) { where(record_index: i) }
   scope :record_at,  ->(i) { find_by(record_index:i ) }
 
+  def self.is_valid_date?(data)
+    begin
+      Date.parse(data.to_s)
+      true
+    rescue
+      false
+    end
+  end
+
   private
   	def log_update
   		logger.debug "DEBUG changes:#{self.changes}"
