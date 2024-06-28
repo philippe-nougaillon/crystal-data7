3f0834a41db82f699609f918a01cfe7d54f10f1f
Philippe NOUGAILLON <philippe.nougaillon@gmail.com>
2024-06-28 15:05:05 +0200

    Stats renamed Graphs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -53,7 +53,7 @@ class PagesController < ApplicationController
 
   def info_notice
     if current_user.compte_démo? && flash[:notice] == nil && flash[:alert] == nil && params.keys.size == 2
-      flash[:notice] = "(i)Les statistiques permettent de visualiser une série de valeurs issues de vos collections"
+      flash[:notice] = "(i)Les graphiques permettent de visualiser une série de valeurs issues de vos collections"
     end
   end
   
