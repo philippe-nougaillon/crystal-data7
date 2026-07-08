597035a60c605f5d42e3410b917d6f9284fb679a
Philippe NOUGAILLON <philippe.nougaillon@gmail.com>
2026-07-08 12:25:48 +0200

    bun fix try #1

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,8 +1,6 @@
 import { Application } from "@hotwired/stimulus"
-import Notification from 'stimulus-notification'
 
 const application = Application.start()
-application.register('notification', Notification)
 
 // Configure Stimulus development experience
 application.debug = false
